efa2f336bc56d8942df99b2c6d207179fae44d8f
nanmardel <martin_n1@denison.edu>
2022-04-14 20:10:35 -0500

    css and features complete

--- controllers/images.js
+++ controllers/images.js
@@ -5,21 +5,36 @@ const Location = require('../models/location');
 module.exports = { 
     addImage,
     delete: deleteImage,
+    editImage,
     updateImage,
+    
 }
-
-
-function updateImage(req, res, next) {
-    Location.findOne({'images._id' : req.params.id}, function(err, locationDoc) {
-        const image = locationDoc.images.id(req.params.id);
-        if(!image.user.equals(req.user.id)) return res.redirect(`/locations/${locationDoc._id}`);
-        image.update();
+ function updateImage(req, res) {
+     console.log('this is a random string');
+    Location.findOne({'images._id': req.params.id}, function(err, locationDoc) {
+        const imageSubdoc = locationDoc.images.id(req.params.id);
+        console.log(imageSubdoc)
+        console.log('this image is being updated')
+        if (!imageSubdoc.user.equals(req.user._id)) return res.redirect(`/locations${locationDoc._id}`);
+        imageSubdoc.description = req.body.description;
+        console.log('image.drescription');
         locationDoc.save(function(err) {
-            if (err) next(err);
-            res.redirect(`/locations/${locationsDoc._id}`);
-        })
-    })
+                res.redirect(`/locations/${locationDoc._id}`);
+        });
+    });
 }
+
+function editImage(req, res) {
+    Location.findOne({'images._id': req.params.id}, function(err, locationDoc) {
+        const image = locationDoc.images.id(req.params.id);
+        console.log(image);
+        console.log('edit this image');
+        res.render('images/edit', {image});
+    });
+}
+
+
+
 
 function deleteImage(req, res, next) {
     Location.findOne({'images._id': req.params.id}, function(err, locationDoc){   
@@ -27,13 +42,16 @@ function deleteImage(req, res, next) {
         req.body.userName = req.user.name;
         req.body.userAvatar = req.user.avatar;
         const image = locationDoc.images.id(req.params.id);
+        console.log(image);
+        console.log('this is an image')
         // if the image wasnt uploaded by the user redirect them back to the same pg
         if(!image.user.equals(req.user.id)) return res.redirect(`/locations/${locationDoc._id}`);
         // remove image
         image.remove();
+        console.log('image has been removed');
         locationDoc.save(function(err) {
             if(err) next(err); 
-            res.redirect(`/locations/${locationsDoc._id}`);
+            res.redirect(`/locations/${locationDoc._id}`);
         });
     });
 }
--- routes/images.js
+++ routes/images.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../config/auth') //add to routes that are only for users that are loggedIn
 const imagesCtrl = require('../controllers/images');
+const updatesCtrl = require('../controllers/images');
 
 
 router.post('/locations/:id/images', isLoggedIn, imagesCtrl.addImage);
 
 router.delete('/images/:id', isLoggedIn, imagesCtrl.delete);
 
-router.put('/images/:id', isLoggedIn, imagesCtrl.updateImage);
+router.get('/images/:id/edit', isLoggedIn, imagesCtrl.editImage);
 
+router.put('/images/:id', isLoggedIn,updatesCtrl.updateImage);
 
 module.exports = router;
 
